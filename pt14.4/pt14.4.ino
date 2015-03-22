36992243a7eb903cee5b2ad656435da7a29d9b31
cesinho15 <cesar_gato.99@hotmail.com>
2015-03-22 19:13:04 +0100

    pt14.4

--- pt14.4/pt14.4.ino
+++ pt14.4/pt14.4.ino
@@ -1,20 +1,44 @@
-int EEE=3; // si variem la variable KgCO2/m2 podem comprovar el funcionamet.
+float EEE = 7.5;
 
-void setup()              // run once, when the sketch starts
+void setup()              // run once. when the sketch starts
 {
-Serial.begin(9600);     // set up Serial library at 9600 bps
-  
-switch (EEE )  { //estructura switch case
-   case A:
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if ( EEE < 3.5)
+  {
     Serial.print("Clase A");
-          //Primera clase
-      break;
-   
-    default: 
-      // if nothing else matches, do the default
-      // default is optional
+  } 
+ 
+ else if ( EEE < 6.5)
+  {
+    Serial.print("Clase B");
+  } 
+
+else if ( EEE < 11.1)
+  {
+    Serial.print("Clase C");
+  } 
+
+else if ( EEE < 17.7)
+  {
+    Serial.print("Clase D");
+  } 
+
+else if ( EEE < 38.2)
+  {
+    Serial.print("Clase E");
+  } 
+
+else if ( EEE < 43.2)
+  {
+    Serial.print("Clase F");
+  } 
+
+else if ( EEE >= 43.2)
+  {
+    Serial.print("Clase G");
   }
-}  
-  void loop()   // we need this to be here even though its empty
+}
+void loop()   // we need this to be here even though its empty
 {
 }
