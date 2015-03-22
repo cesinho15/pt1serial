81383835ef788b7752fe31ffa9ecbd18344b6a8f
cesinho15 <cesar_gato.99@hotmail.com>
2015-03-22 15:18:01 +0100

    pt1-1.1

---
+++ pt1-1/pt1-1.1.ino
@@ -0,0 +1,11 @@
+void setup()                // executar nomes 1 cop al començar la simulació
+{
+  Serial.begin(9600);       // estableix la velocitat de dades per transmisió 9600 bps
+  Serial.println("Hello world!"); // imprimeix "hello word!" en format de text
+}
+
+void loop()   // s'executa tota l'estona
+{
+		   // no hi ha res connectat
+}
+
